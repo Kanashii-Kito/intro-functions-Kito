ef081055c57d7c79aec62e348492f676317395af
Kanashii-Kito <ERicc468@nycstudents.net>
2025-09-10 10:30:27 -0400

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,55 @@ from turtle import *
 t = Turtle()
 
 
-t.shape('turtle')
+# turtle.done()
 
-def square(x):
+# def add(x,y):
+#     print(x + y)
+# add(15,5)
+# add(7,8)
+
+# def square(x):
+#     t.foward(x)
+#     t.left(90)
+#     t.foward(x)
+#     t.left(90)
+#     t.foward(x)
+#     t.left(90)
+#     t.foward(x)
+#     t.left(90)
+
+
+#     for i in range(0,4):
+#         #do something
+#         print(i)
+
+# def rect():
+# for  i in range(0,4):
+
+#     t.forward(125)
+#     t.left(90)
+
+
+# rect()
+
+
+
+
+#that is squre below:D
+# for i in range(0,4):
+#     t.width(5)
+#     x=100
+#     t.forward(x)
+#     t.left(90)
+
+# turtle.done()
+
+for i in range(0,4):
+    t.width(5)
+    x=100
     t.forward(x)
     t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-    t.forward(x)
-    t.left(90)
-square(200)
 
 turtle.done()
+
+
